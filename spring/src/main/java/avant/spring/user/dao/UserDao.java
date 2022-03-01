a5febd2da172057258f82083197c74f23819d4e6
semo41105 <semo41105@gmail.com>
2022-03-01 15:31:54 +0900

    ~3.6

--- spring/src/main/java/avant/spring/user/dao/UserDao.java
+++ spring/src/main/java/avant/spring/user/dao/UserDao.java
@@ -2,28 +2,38 @@ package avant.spring.user.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
 
 import avant.spring.user.domain.User;
 
 public class UserDao {
-	private DataSource dataSource;
+	private JdbcTemplate jdbcTemplate;
+	private RowMapper<User> rowMapper = new RowMapper<User>() {
+		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+			User user = new User();
+			user.setId(rs.getString("ID"));
+			user.setName(rs.getString("NAME"));
+			user.setPassword(rs.getString("PASSWORD"));
+			return user;
+		}
+	};
 
 	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
-		jdbcContext = new JdbcContext();
-		jdbcContext.setDataSource(dataSource);
+		jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
-	private JdbcContext jdbcContext;
-
 	public void add(final User user) throws SQLException {
-		jdbcContext.workWithStatementStrategy(new StatementStrategy() {
-
-			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-				PreparedStatement ps = c.prepareStatement(
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				PreparedStatement ps = con.prepareStatement(
 						"insert into users(id, name, password) values(?,?,?)");
 				ps.setString(1, user.getId());
 				ps.setString(2, user.getName());
@@ -34,6 +44,25 @@ public class UserDao {
 	}
 
 	public void deleteAll() throws SQLException {
-		jdbcContext.execute("delete from users");
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				return con.prepareStatement("delete from users");
+			}
+		});
+
+		jdbcTemplate.update("delete from users");
 	}
+
+	public int getAll() {
+		return jdbcTemplate.queryForObject("SELECT COUNT(*) FROM users", Integer.class);
+	}
+
+	public User getOne(String id) {
+		return jdbcTemplate.queryForObject("SELECT * FROM USERS WHERE id = ?", rowMapper, id);
+	}
+
+	public List<User> getAllUser() {
+		return jdbcTemplate.query("SELECT * FROM USERS", rowMapper);
+	}
+
 }
